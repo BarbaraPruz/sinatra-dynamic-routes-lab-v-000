85af72a2d23c23a544d00ea3a4ca63484b625211
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-05-16 23:39:41 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,6 +12,9 @@ class App < Sinatra::Base
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     params[:word1] +params[:word2] +params[:word3] +params[:word4] +params[:word5]
   end
+  get '/:operation/:number1/:number2'  do
+    "#{params[:operation]} - #{params[:number1]} - #{params[:number2]} "
+  end 
   get '/say/:number/:phrase' do
     params[:phrase] * params[:number].to_i
   end
